a116b724ad02a571782bb1b8c5b0f304c9ef6078
Leo Cheung <lappangcheung@gmail.com>
2023-11-19 16:24:47 -0500

    Added the home info component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Plane from '../models/Plane'
 import Bird from '../models/Bird'
 import Sky from '../models/Sky'
 import Island from '../models/Island'
+import HomeInfo from '../components/HomeInfo'
 
 const Home = () => {
 
@@ -48,6 +49,9 @@ const Home = () => {
 
 	return (
 		<section className="w-full h-screen relative">
+			<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+				{currentStage && <HomeInfo currentStage={currentStage} />}
+			</div>
 			<Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
 			        camera={{ near: 0.1, far: 1000}}>
 				<Suspense fallback={<Loader />}>
